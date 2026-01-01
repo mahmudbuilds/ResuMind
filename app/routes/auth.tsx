b65c5531ae01edcc2805d11254332f3aa2f03943
Mahmud <mahxgithub@gmail.com>
2026-01-01 10:21:35 +0100

    feat: Implement core application structure including UI components, routes, data types, constants, and Puter.js integration for the AI resume analyzer.

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,24 +1,54 @@
-export const meta = () => (
-    [
-        {title: 'ResuMind | Auth'},
-        {name: 'description', content: 'Log into your account'}
-    ]
-)
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter";
 
+export const meta = () => [
+  { title: "ResuMind | Auth" },
+  { name: "description", content: "Log into your account" },
+];
 
 export default function Auth() {
-    return (
-        <div>
-            <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
-                <div className="gradient-border shadow-lg">
-                    <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
-                        <div className="flex flex-col items-center gap-2 text-center">
-                            <h1>Welcome</h1>
-                            <h2>Log In to Continue Your Job Journey</h2>
-                        </div>
-                    </section>
-                </div>
-            </main>
+  const { isLoading, auth } = usePuterStore();
+  const location = useLocation();
+  const next = location.search.split("next=")[1];
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (auth.isAuthenticated) {
+      navigate(next);
+    }
+  }, [auth.isAuthenticated, next]);
+  return (
+    <div>
+      <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
+        <div className="gradient-border shadow-lg">
+          <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
+            <div className="flex flex-col items-center gap-2 text-center">
+              <h1>Welcome</h1>
+              <h2>Log In to Continue Your Job Journey</h2>
+            </div>
+            <div>
+              {isLoading ? (
+                <button className="auth-button animate-pulse">
+                  Signing you in...
+                </button>
+              ) : (
+                <>
+                  {auth.isAuthenticated ? (
+                    <button className="auth-button" onClick={auth.signOut}>
+                      Log Out
+                    </button>
+                  ) : (
+                    <button className="auth-button" onClick={auth.signIn}>
+                      Log In
+                    </button>
+                  )}
+                </>
+              )}
+            </div>
+          </section>
         </div>
-    );
+      </main>
+    </div>
+  );
 }
